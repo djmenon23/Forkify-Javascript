dd03aa13f5c2ea90e5c4dc035fe058be95543766
dhananjay <dhananjaymenon23@gmail.com>
2018-10-18 23:31:13 +0530

    Added ajax spinner and limited results diplayed in the page

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader} from './views/base';
 /**  Global State of the app
  * -Search Object
  * -Current recipw Object
@@ -21,10 +21,12 @@ const controlSearch = async () => {
         //3) Prepare Ui for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
         //4) Search for recepies
         await state.search.getResults();
 
         //5) render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 };
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -9,8 +9,30 @@ export const clearInput = () => {
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
-
-
+/*
+Example of how the function logic will work
+'Pasta with tomato and spinach'
+first iteration
+acc: 0 / acc + cur.length = 5: newTitle = ['Pasta']
+acc: 5 / acc + cur.length = 9: newTitle = ['Pasta', 'with']
+acc: 9 / acc + cur.length = 15: newTitle = ['Pasta', 'with','Tomato']
+acc: 15 / acc + cur.length = 18: newTitle = ['Pasta', 'with','Tomato']
+acc: 18 / acc + cur.length = 24: newTitle = ['Pasta', 'with','Tomato']
+*/
+const limitRecipeTitle = (title, limit = 17) => {
+    //0 is the initial value of the accumulator(acc)
+    const newTitle = [];
+    if(title.length > limit) {
+        title.split(' ').reduce((acc, cur) =>{
+            if(acc + cur.length <= limit){
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        },0);
+        return `${newTitle.join(' ')}...`;
+    };
+    return title;
+}
 
 const renderRecipe = recipe => {
     const markup = `
@@ -20,7 +42,7 @@ const renderRecipe = recipe => {
             <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
@@ -29,6 +51,38 @@ const renderRecipe = recipe => {
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe)
+const createButton = (page, type) => `     <!--
+<button class="btn-inline results__btn--prev">
+    <svg class="search__icon">
+        <use href="img/icons.svg#icon-triangle-left"></use>
+    </svg>
+    <span>Page 1</span>
+</button>
+<button class="btn-inline results__btn--next">
+    <span>Page 3</span>
+    <svg class="search__icon">
+        <use href="img/icons.svg#icon-triangle-right"></use>
+    </svg>
+</button>
+-->`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
+    if(page === 1 && pages > 1){
+        //Only button to go to next page
+    }
+    else if(page < pages) {
+        //Both Buttons
+    }
+    else if(page === pages && pages > 1){
+        //Only button to go to orevious page
+
+    }
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+    recipes.slice(start, end).forEach(renderRecipe)
 };
